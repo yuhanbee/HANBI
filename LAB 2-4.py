07136a71e861d8c1e2f8aab06b18d9f79fcbce2c
yuhanbee <gksql1734@naver.com>
2025-03-12 20:39:08 +0900

    test

---
+++ LAB 2-4.py
@@ -0,0 +1,10 @@
+name = '전우치'
+print('나의 이름은 :', name)
+age = 27
+print('나의 나이는 :', age)
+height = 179
+print('나의 키는', height, 'cm 입니다.')
+sum = 10 + 20
+print('10 + 20=', sum)
+mult = 10 * 20
+print('10 * 20=', mult)
